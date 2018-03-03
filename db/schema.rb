7e0927ec58057f1cdbbe08ee7ad5d19c668eb606
野見山 <nomiyama@planningdevnoMacBook-Pro.local>
2018-03-03 15:27:03 +0900

    signup画面の整形

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20180303025135) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "name"
     t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
