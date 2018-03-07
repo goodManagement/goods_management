2c0c6b01a4fbfd65ef671d2b57def70065e5b92d
野見山 <nomiyama@planningdevnoMacBook-Pro.local>
2018-03-07 20:31:17 +0900

    signup画面の設計３

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,34 +1,3 @@
 module ApplicationHelper
 
 end
-
-def devise_error_messages
-  
-    return "" if resource.errors.empty?
-    html = ""
-    # エラーメッセージ用のHTMLを生成
-    messages = resource.errors.full_messages.each do |msg|
-      html += <<-EOF
-        <div class="error_field " role="alert">
-          <p class="error_msg">・#{msg}</p>
-        </div>
-      EOF
-    end
-    html.html_safe
-end
-
-def error_messages
-
-    return "" if resource.errors.empty?
-    html = ""
-    # エラーメッセージ用のHTMLを生成
-    messages = resource.errors.full_messages.each do |msg|
-      html += <<-EOF
-        <div class="error_field" role="alert">
-          <p class="error_msg">・#{msg}</p>
-        </div>
-
-      EOF
-    end
-    html.html_safe
-end
