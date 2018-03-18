c3b68b1d3adeed735b6ddb8044a74a1a5306412b
pokotsun <gorigolila@gmail.com>
2018-03-18 16:39:33 +0900

    i1 Seed用のデータを作った

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :lendings
 
   # 今現在借りている(is_lent: trueである)lendingを返す
-  def current_lending
+  def current_lendings
     lendings.order(:updated_at).select { |lending| lending.is_lent == true }
   end
 
