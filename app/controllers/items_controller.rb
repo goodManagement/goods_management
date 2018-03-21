a39cb54b5351af8e1ecf79a3163077dacd124a79
tAkuMiNL <takaramono0906@gmail.com>
2018-03-21 17:04:36 +0900

    items#indexの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,6 @@ class ItemsController < ApplicationController
       item = Item.new(serial_number:0000+i, name:"item#{i}", kind:i)
       @items.push(item)
     end
-    p "Items#index"
-    # p @items
     render :index, layout: "application_with_navbar"
   end
 
@@ -25,9 +23,6 @@ class ItemsController < ApplicationController
     elsif search_name.present?
       @items = @items.where("name like '%#{search_name}%'")
     end
-    p "Items#search"
-
-    p @items
     render :index, layout: "application_with_navbar"
   end
 end
