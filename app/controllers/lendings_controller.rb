3f08f76ed2e293a1d0dc90eff90aa2c563f0895f
野見山 <nomiyama@planningdevnoMacBook-Pro.local>
2018-03-22 14:39:30 +0900

    modal

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -1,15 +1,15 @@
 class LendingsController < ApplicationController
   def create
-    @lending = Lending.new(user_id: current_user.id, item_id:params[:serial_number] , dead_line:params[:dead_line], is_lent:true)
+    @lending = Lending.new(user_id: params[:lending][:user_id], item_id: params[:lending][:item_id], dead_line:params[:lending][:dead_line], is_lent:true)
    if @lending.save
       flash[:notice]="アイテムが作成されました"
-       redirect_to("/")
+       redirect_to controller: 'home', action: 'index'
    else
      messages = ""
      @lending.errors.full_messages.each{|msg| messages += "#{msg}¥n"}
 
      flash[:alert]= messages
-      redirect_to("/")
+      redirect_to  controller: 'home', action: 'index'
    end
 
  end
