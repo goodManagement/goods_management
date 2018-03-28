d872b0b50965212ade46bbb6ece968e4e9452e23
pokotsun <gorigolila@gmail.com>
2018-03-29 01:41:05 +0900

    Devisehelperの管理

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,22 +1,24 @@
 module DeviseHelper
+
   # deviseのエラーメッセージを表示する
   def devise_error_messages!
-      return "" if resource.errors.empty? and flash.empty?
-      html = ""
+    return "" if resource.errors.empty? and flash.empty?
 
-      # エラーメッセージ用のHTMLを生成
-      messages = resource.errors.full_messages
-      if flash.present?
-        flash.each { |type, msg|  messages.push(msg) }
-      end
+    html = ""
+    # エラーメッセージ用のHTMLを生成
+    messages = resource.errors.full_messages
+    if flash.present?
+      flash.each { |type, msg|  messages.push(msg) }
+    end
 
-      messages.each do |msg|
-        html += <<-EOF
-          <div class="error_field" role="alert">
-            <p class="error_msg">・#{msg}</p>
-          </div>
-        EOF
-      end
-      html.html_safe
+    messages.each do |msg|
+      html += <<-EOF
+      <div class="error_field" role="alert">
+      <p class="error_msg">・#{msg}</p>
+      </div>
+      EOF
+    end
+    html.html_safe
   end
+  
 end
